9f339b52ef242b17557fa065add0f895d232e94a
Juanjo <69094654+Juanjo-Zorrilla@users.noreply.github.com>
2021-05-21 21:53:04 -0300

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById('btn_menu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');  
+});
